c59dda1906f79908ec376b430cf3d07c28850581
Matheus Bazoti <mbazoti@outlook.com.br>
2024-02-20 11:18:24 -0300

    gera recomendação para o usuário baseado no parecer

--- main.py
+++ main.py
@@ -113,10 +113,46 @@ def gerar_parecer(transacao):
     print("Finalizou a geração de parecer")
     return conteudo
 
+def gerar_recomendacao(parecer):
+    print("3. Gerando recomendações")
+    
+    prompt_sistema = f"""
+    Para a seguinte transação, forneça uma recomendação apropriada baseada no status e nos detalhes da transação da Transação: {parecer}
+
+    As recomendações podem ser "Notificar Cliente", "Acionar setor Anti-Fraude" ou "Realizar Verificação Manual".
+    Elas devem ser escritas no formato técnico.
+
+    Inclua também uma classificação do tipo de fraude, se aplicável. 
+    """
+    
+    lista_mensagens = [
+        {
+            "role" : "system",
+            "content" : prompt_sistema
+        }
+    ]
+    
+    resposta = cliente.chat.completions.create(
+        messages=lista_mensagens,
+        model=modelo
+    )
+    
+    conteudo = resposta.choices[0].message.content
+    print("Finalizou a geração de recomendação")
+    return conteudo
+
+
+
+### Analisa as transações
+
 lista_transacoes = carrega("dados/transacoes.csv")
 transacoes_analisadas = analisar_transacao(lista_transacoes)
 
 for transacao in transacoes_analisadas["transacoes"]:
     if transacao["status"] == "Possivel Fraude":
         parecer = gerar_parecer(transacao)
-        print(parecer)
+        recomendacao = gerar_recomendacao(parecer)
+        id_transacao = transacao["id"]
+        produto_transacao = transacao["nome_produto"]
+        status_transacao = transacao["status"]
+        salva(f"transacao-{id_transacao}-{produto_transacao}-{status_transacao}.txt", recomendacao)
